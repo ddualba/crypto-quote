d00f44021bfb083ece083b9315dd0a0702029dc8
Dar Dualba <ddualba@gmail.com>
2021-07-28 00:00:37 -0700

    update server.js to serve static assets in production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,5 +12,15 @@ app.use(express.json({ extended: false })); // (Provides access to req.body)
 
 // Define routes
 app.use('/', require('./routes/coins'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
